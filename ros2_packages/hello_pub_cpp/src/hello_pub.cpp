0e26dbdaf3667d540a08db5b0da49ad1b63b3894
Kensuke Saito <ken.saito.0813@gmail.com>
2020-04-18 21:33:13 +0900

    Update hello_pub.cpp

--- ros2_packages/hello_pub_cpp/src/hello_pub.cpp
+++ ros2_packages/hello_pub_cpp/src/hello_pub.cpp
@@ -11,20 +11,21 @@ class HelloPub : public rclcpp::Node
 public:
 	HelloPub();
 private:
+	std::shared_ptr<std_msgs::msg::String> msg;
 	void timerCB();
 	rclcpp::TimerBase::SharedPtr timer;
 	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub;
 };
 HelloPub::HelloPub() : Node("hello_pub")
 {
+	msg = std::make_shared<std_msgs::msg::String>();
 	pub = this->create_publisher<std_msgs::msg::String>("/hello", 10);
 	timer = this->create_wall_timer(100ms, std::bind(&HelloPub::timerCB, this));
 }
 
 void HelloPub::timerCB()
 {
-	auto msg = std_msgs::msg::String();
-	msg.data = "Hello World!";
+	msg->data = "Hello World!";
 	RCLCPP_INFO(this->get_logger(), "Publishing: %s", msg.data.c_str());
 	pub->publish(msg);
 }
